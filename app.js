c4a1328a442cc331a88c205d164386655c036c3c
anabrd <ana.d.brdar@gmail.com>
2021-06-20 21:32:21 +0200

    add edit nickname func

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 // - Add channel option
 // - Remove user from list when they logout
 // - Add private messaging
+// - Sort user array so current user is always on top, customize user color
+// - Make general the default channel
+
 
 const express = require("express");
 const app = express();
@@ -18,10 +21,11 @@ app.use(express.static(__dirname + "/public"));
 
 const channels = ["General", "Work", "Hobbies"];
 const users = [];
+const usersMap = {};
+
+
 
 io.on('connection', (socket) => {
-
-
 
     socket.on('token', token => {
 
@@ -35,6 +39,7 @@ io.on('connection', (socket) => {
             if (existCheck.length == 0) {
                 users.push(nickName);
             }
+            usersMap[nickName] = socket;
             return ticket;
         }
 
@@ -59,15 +64,29 @@ io.on('connection', (socket) => {
         socket.emit('channels', channels);
     })
 
-    socket.on('chat message', envelope => {
-        // if (envelope.user) {
-        //     // message will go to user
-        // } else {
+    socket.on('send message', envelope => {
+        console.log(envelope)
+        if (envelope.toUser) {
+            //message will go to user
+            socket.emit('receive message', envelope);
+            usersMap[envelope.toUser].emit('receive message', envelope);
+        } else {
             // This will send the message back to all clients (that's why we use io instead of socket)
-            io.emit('chat msg', envelope)
-        // }
-        
+            io.emit('receive message', envelope)
+        }    
     })
+
+    socket.on('edit nickname', nickObj => {
+        //Filter for oldnickname, find index, splice the array at that index and insert newNickname
+        let oldNick = nickObj.oldNick;
+        let index = users.indexOf(oldNick);
+        nickName = nickObj.newNick
+        users.splice(index, 1, nickName);
+        let token = jwt.sign({nickName}, 'fsdkjfniw3y3424', {expiresIn: '1d'});
+        const ticket = {token, nickName};
+        socket.emit("ticket", ticket);
+        io.emit('users', users);
+    });
 
     socket.on('disconnect', () => {
         console.log('A user disconnected.');
--- public/main.js
+++ public/main.js
@@ -4,50 +4,110 @@ const state = {
     list: 'channels',
     users: [],
     channels: [],
-    activeChannel: 'General',
+    activeChannel: null,
     activeUser: null,
-    usedChannels: []
+    toUser: null,
+    usedChannels: [],
+    usedDMs: []
 }
+
+const editNickname = () => {
+    document.querySelector("#edit-nickname").style.display = "block"
+    document.querySelector("#card-main").classList.remove("card-tertiary");
+}
+
+const cancelNickname = () => {
+    document.querySelector("#edit-nickname").style.display = "none"
+    document.querySelector("#card-main").classList.add("card-tertiary");
+}
+
+const submitNickname = () => {
+    let editedNick = {oldNick: state.activeUser, newNick: document.querySelector("#new-nickname").value};
+    console.log(editedNick)
+    socket.emit('edit nickname', editedNick);
+    cancelNickname();
+}
+
+const addChannel = () => {
+    document.querySelector("#add-channel").style.display = "block"
+    document.querySelector("#card-main").classList.remove("card-tertiary");
+}
+
+const cancelChannel = () => {
+    console.log("clicked")
+    document.querySelector("#add-channel").style.display = "none"
+    document.querySelector("#card-main").classList.add("card-tertiary");
+}
+
 
 const clickHandler = () => {
     let msg = document.querySelector("#msg-box");
-    console.log("ok");
-    let envelope =  {msg: msg.value, channel: state.activeChannel, user: state.activeUser};
-    socket.emit("chat message", envelope);
+    let envelope =  {msg: msg.value, channel: state.activeChannel, fromUser: state.activeUser, toUser: state.toUser};
+    socket.emit("send message", envelope);
     console.log(envelope)
     msg.value = '';
 }
 
 const listItemSelect = (name) => {
-    console.log(name);
-    const preItem = state.activeChannel;
-    state.activeChannel = name;
-    document.querySelector(`#${preItem}`).classList.remove('active-li');
-    document.querySelector(`#${name}`).classList.add('active-li');
-    if(!state.usedChannels[name]){
+    let preItem;
+    console.log(state.list, "state.list before if/else")
+    // FOR CHANNELS
+    if (state.list == "channels") {
+        state.toUser = null;
+        console.log("state.activeChannel", state.activeChannel);
+        
+        if (state.activeChannel) {
+            preItem = state.activeChannel;
+            document.querySelector(`#${preItem}`).classList.remove('active-li');
+        }
+        
+        state.activeChannel = name;
+        document.querySelector(`#${name}`).classList.add('active-li');
+        
+        if (!state.usedChannels[name]) {
             state.usedChannels[name] = [];
         }
-        console.log(state.usedChannels);
-
-        if (state.list == "channels") {
-            document.querySelector('#chat-box').innerHTML = `<li class="text-info">Welcome to <b>#${state.activeChannel}</b> channel!`;
-        } else {
-            document.querySelector('#chat-box').innerHTML = `<li class="text-info">Here is where you can send #${state.activeChannel} a private message.`;
-        }
-        state.usedChannels[state.activeChannel].forEach( msg => {
+        
+        document.querySelector('#chat-box').innerHTML = `<li class="text-info">Welcome to <b>#${state.activeChannel}</b> channel!`;
+        state.usedChannels[state.activeChannel].forEach(msg => {
             document.querySelector('#chat-box').insertAdjacentHTML("beforeend", `<li>${msg}</li>`);
-    });
+        })
+
+    // FOR DMS
+    } else {
+        if (state.toUser) {
+            preItem = state.toUser;
+            document.querySelector(`#${preItem}`).classList.remove('active-li');
+        }
+        
+        console.log("state toUser before", state.toUser)
+        state.toUser = name;
+        console.log("state toUser after", state.toUser)
+        document.querySelector(`#${name}`).classList.add('active-li');
+        
+        document.querySelector('#chat-box').innerHTML = `<li class="text-info">Here is where you can send #${state.toUser} a private message.<li>`;
+        
+        //state.usedDMs[state.toUser].forEach(msg => {
+            //document.querySelector('#chat-box').insertAdjacentHTML("beforeend", `<li><span class="text-nick">${state.toUser}:</span>${msg}</li>`);
+        //})
+    }
 }
 
 function listToggler(activeList) {
     document.querySelector("#user-channel-list").innerHTML = "";
     state.list = activeList;
+
     state[state.list].forEach(item => {
         document.querySelector("#user-channel-list").insertAdjacentHTML("beforeend", `<li id="${item}" class="li-item" onclick="listItemSelect('${item}')">${item}</li>`);
+        if (item == state.activeUser) {
+            document.querySelector(`#${item}`).insertAdjacentHTML("beforeend", `<span class="thats-you"> (That's you!)</span>`)
+        }
     })
 
     if (activeList == "channels") {
-        document.querySelector("#user-channel-list").insertAdjacentHTML("beforeend", `<button id="add-channel-btn" class="btn col-lg-12 btn-primary" onclick="">Add New Channel</li>`);
+        document.querySelector("#user-channel-list").insertAdjacentHTML("beforeend", `<button id="add-channel-btn" class="btn col-lg-12 btn-primary" onclick="addChannel()">Add New Channel</li>`);
+    } else {
+        document.querySelector("#user-channel-list").insertAdjacentHTML("beforeend", `<button id="add-channel-btn" class="btn col-lg-12 btn-primary" onclick="editNickname()">Edit Nickname</li>`);
     }
 }
 
@@ -59,17 +119,38 @@ socket.on('ticket', ticket => {
     state.activeUser = ticket.nickName;
 })
 
-socket.on('chat msg', envelope => {
-    Object.keys(state.usedChannels).forEach( key => {
-        if(envelope.channel == key) {
-            state.usedChannels[key].push(envelope.msg);
-        }
-        console.log(state.usedChannels[key]);
-    });
+socket.on('receive message', envelope => {
+    if (envelope.toUser) {
 
-    if (state.activeChannel == envelope.channel) {
-        document.querySelector('#chat-box').insertAdjacentHTML("beforeend", `<li><span class="text-nick">${envelope.user}:</span> ${envelope.msg}</li>`);
-    }
+        if (!state.usedDMs[envelope.toUser]) {
+            state.usedDMs[envelope.toUser] = [];
+        }
+
+        Object.keys(state.usedDMs).forEach(key => {
+            if (envelope.toUser == key) {
+                state.usedDMs[key].push(envelope.msg);
+            }
+            console.log(state.usedDMs[key]);
+        });
+
+        if (state.activeUser == envelope.fromUser) {
+            document.querySelector('#chat-box').insertAdjacentHTML("beforeend", `<li><span class="text-nick">${envelope.fromUser}: </span>${envelope.msg}</li>`);
+        }
+        console.log("state", state)
+    } else {
+        Object.keys(state.usedChannels).forEach(key => {
+            if (envelope.channel == key) {
+                state.usedChannels[key].push(envelope.msg);
+            }
+            console.log(state.usedChannels[key]);
+        })
+
+        if (state.activeChannel == envelope.channel) {
+            document.querySelector('#chat-box').insertAdjacentHTML("beforeend", `<li><span class="text-nick">${envelope.fromUser}:</span> ${envelope.msg}</li>`);
+        }
+    };
+
+    
 });
 
 
